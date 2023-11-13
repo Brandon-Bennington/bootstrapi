a5754c40573f5609edfc3db52841958d2d8a9148
brandon-bennington <brandon.bennington@codeimmersives.com>
2023-11-13 19:37:36 -0300

    Completed

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.btn-primary');
-    const image = document.querySelector('.card-img-top');
+    const dogButton = document.querySelector('.btn-primary');
+    const weatherButton = document.getElementById('getWeather');
 
-    button.addEventListener('click', () => {
+    dogButton.addEventListener('click', () => {
         fetch('https://dog.ceo/api/breeds/image/random')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
-                image.src = data.message;
-                image.alt = "Random Dog Image";
+                addSlideToCarousel('#dogCarousel', data.message);
             })
             .catch(error => {
                 console.error('Error fetching the dog image:', error);
-                image.src = 'img/PlaceHolderDog.png'; // Placeholder image on error
+                addSlideToCarousel('#dogCarousel', 'img/PlaceHolderDog.png');
             });
     });
+
+    weatherButton.addEventListener('click', () => {
+        const city = document.getElementById('cityInput').value;
+        const url = `https://goweather.herokuapp.com/weather/${city}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const weatherContent = `
+                    <div>
+                        <h3>Weather in ${city}</h3>
+                        <p>Temperature: ${data.temperature}</p>
+                        <p>Wind: ${data.wind}</p>
+                        <p>Description: ${data.description}</p>
+                    </div>`;
+                addSlideToCarousel('#weatherCarousel', weatherContent);
+            })
+            .catch(error => {
+                console.error('Error fetching weather:', error);
+            });
+    });
+
+    function addSlideToCarousel(carouselId, content) {
+        const carouselInner = document.querySelector(`${carouselId} .carousel-inner`);
+        const newSlide = document.createElement('div');
+        newSlide.className = 'carousel-item';
+        if (carouselInner.children.length === 0) {
+            newSlide.classList.add('active');
+        }
+        if(carouselId === '#dogCarousel') {
+            newSlide.innerHTML = `<img src="${content}" class="d-block w-100" alt="Random Dog">`;
+        } else {
+            newSlide.innerHTML = content;
+        }
+        carouselInner.appendChild(newSlide);
+    }
 });
-
-document.getElementById('getWeather').addEventListener('click', () => {
-    const city = document.getElementById('cityInput').value;
-    const url = `https://goweather.herokuapp.com/weather/${city}`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const display = document.getElementById('weatherDisplay');
-            display.innerHTML = `
-                <p>Temperature: ${data.temperature}</p>
-                <p>Wind: ${data.wind}</p>
-                <p>Description: ${data.description}</p>
-            `;
-        })
-        .catch(error => {
-            console.error('Error fetching weather:', error);
-            document.getElementById('weatherDisplay').innerHTML = `<p>Error fetching weather data.</p>`;
-        });
-});
-
